9e70be0087dac6e9ce31eb25be67d539a0d2ab47
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-12 07:32:44 +0300

    issue-3 wip 7

--- src/python/inference_core/nonparametric.py
+++ src/python/inference_core/nonparametric.py
@@ -4,10 +4,10 @@ from math import ceil,floor
     
 def calculate_empirically_cdf(sample, t, total):
     print(f"calculate_empirically_cdf: caculating cdf from [{len(sample)}] sample with threshold {t}")
-    return len([s for s in sorted(sample) if s[1] < t]) / total
+    return len([s for s in sample if s[1] <= t]) / len(sample)
 
 def calculate_plug_in_quantile(sample, q_value):
-        
-    return sorted(sample, key=lambda i: i[1])[floor(q_value * len(sample))]
+    index =     min(ceil(q_value * len(sample)),len(sample)-1)
+    return sorted(sample)[index]
 
 
