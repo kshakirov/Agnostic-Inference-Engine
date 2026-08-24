3b16c6d344f08e4e61d723f11246d33fde13bfbc
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-08-24 08:48:04 +0300

    experiment: sample Gaussian MLE estimates

--- src/python/test_mle.py
+++ src/python/test_mle.py
@@ -1,4 +1,21 @@
-from inference_core.mle.generator import generate_normal_stream
+from inference_core.mle.generator import generate_normal_stream, mean_std
 
-stream = generate_normal_stream(1000, 150, 1000)
-stream[0:10]
+
+
+
+estms = []
+for i in range(10):
+    s = generate_normal_stream(1000, 150, 10000,i )
+    m,std = mean_std(s)
+    print(m,std)
+    estms.append((m,std))
+
+
+means = [m for (m,std) in estms]
+stds = [std for (m,std) in estms]
+
+params_of_means = mean_std(means)
+params_of_stds = mean_std(stds)
+
+print(f"params of means {params_of_means}")
+print(f"params of stds {params_of_stds}")
