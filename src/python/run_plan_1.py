f8fe9742b891f5232319c62f596b12f22d7d0268
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-09-01 07:43:03 +0300

    ecdf in progress

--- src/python/run_plan_1.py
+++ src/python/run_plan_1.py
@@ -2,6 +2,18 @@ from  inference_core.wasserman import calculate_empirical_moments
 import numpy as np
 import array
 
+
+def empiricall_cdf(sample):
+    print(f"_empirically_cdf: caculating cdf from [{len(sample)}] ")
+    x = np.sort(sample)
+    y_direct = np.arange(len(x))
+    y_rev = y_direct[::-1]
+    y_i = np.divide(y_direct,y_rev)
+    y= np.multiply(x,y_i)
+    return y 
+    
+    
+
 print("Followng the plan")
 fd = open("/Users/kiryloshakirov/Documents/ChatGPT/Agnostic-Inference-Engine/experiments/latency/2026-08-31-baseline-a/baseline-a.csv")
 print(fd)
@@ -23,4 +35,7 @@ mean = np.mean(np_data)
 var  = np.var(np_data)
 std = np.std(np_data)
 
-print(f"mean{mean}, var {var},  std {std}")
+print(f"mean = {mean}\n var =  {var}\n std =  {std}")
+
+
+empiricall_cdf(np_data)
