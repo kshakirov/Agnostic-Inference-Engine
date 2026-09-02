882cf2db0a9bb86d5c794666236baebcf9ac2046
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-09-02 07:22:58 +0300

    wip 2

--- src/python/run_plan_1.py
+++ src/python/run_plan_1.py
@@ -6,36 +6,35 @@ import array
 def empiricall_cdf(sample):
     print(f"_empirically_cdf: caculating cdf from [{len(sample)}] ")
     x = np.sort(sample)
-    y_direct = np.arange(len(x))
-    y_rev = y_direct[::-1]
-    y_i = np.divide(y_direct,y_rev)
-    y= np.multiply(x,y_i)
-    return y 
+    y_direct = np.arange(1, len(x) +1 )
+    y_len = np.full(len(x),len(x))
+    y_i = np.divide(y_direct,y_len)
+#    y= np.multiply(x,y_i)
+    return x,y_i
     
     
 
-print("Followng the plan")
-fd = open("/Users/kiryloshakirov/Documents/ChatGPT/Agnostic-Inference-Engine/experiments/latency/2026-08-31-baseline-a/baseline-a.csv")
-print(fd)
+file_name = "/Users/kiryloshakirov/Documents/ChatGPT/Agnostic-Inference-Engine/experiments/latency/2026-08-31-baseline-a/baseline-a.csv"
+length = 600
 
+def prep_data(file_name, length):
+    fd = open(file_name)
+    np_data = np.zeros(length,dtype=np.float64)
+    print(fd)
+    for i, line in enumerate(fd):
+        cells = line.split(',')
+        np_data[i] = float(cells[2])/1000000
+    return np_data
 
-np_data = np.zeros(600,dtype=np.float64)
-
-
-for i, line in enumerate(fd):
-    cells = line.split(',')
-#    print(float(cells[]))
-    np_data[i] = float(cells[2])/1000000
-#    break
-#data[:10]
-
-np_data[0:10]
+np_data = prep_data(file_name, length)
+#print(np_data)
 mean = np.mean(np_data)
-# mean
+
 var  = np.var(np_data)
 std = np.std(np_data)
 
 print(f"mean = {mean}\n var =  {var}\n std =  {std}")
 
 
-empiricall_cdf(np_data)
+x, y = empiricall_cdf(np_data)
+print(f"ecdf x 10  = {x[0:10]} y 10 = {y[0:10]}")
--- src/python/test_mle.py
+++ src/python/test_mle.py
@@ -1,13 +1,16 @@
 from inference_core.mle.generator import generate_normal_stream, mean_std
+from math import sqrt
+import numpy as np
 
-
-
-
+n = 10000
+mu = 1000
+sigma = 150
 estms = []
-for i in range(10):
-    s = generate_normal_stream(1000, 150, 10000,i )
+viborka =1000
+for i in range(viborka):
+    s = generate_normal_stream(mu, sigma, n,i )
     m,std = mean_std(s)
-    print(m,std)
+#    print(m,std)
     estms.append((m,std))
 
 
@@ -19,3 +22,18 @@ params_of_stds = mean_std(stds)
 
 print(f"params of means {params_of_means}")
 print(f"params of stds {params_of_stds}")
+
+std_theoretic = sigma / sqrt(n)
+
+print(std_theoretic)
+
+fisher_info = 1 / sigma ** 2
+
+fisher_info_n = fisher_info * n
+se_fisher_info = 1 /sqrt(fisher_info_n)
+print(f"Fisher Info is {fisher_info}, for n is {fisher_info_n}, SE Fisher Info {se_fisher_info}")
+
+disps = [std*std for std in stds]
+disps[0:5]
+disp_mean = mean_std(disps)
+disp_mean
