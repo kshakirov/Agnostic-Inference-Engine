b73f256d3bbb94f6ccd06eec7c305e9345b6271a
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-09-16 07:27:42 +0300

    refactoring 23

---
+++ src/python/inference_core/utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+def prep_data(file_name, length):
+    """ получить эпирические данные  from file"""
+    np_data = np.zeros(length,dtype=np.float64)
+    with open(file_name) as fd:
+        print(fd)
+        for i, line in enumerate(fd):
+            cells = line.split(',')
+            np_data[i] = float(cells[2])/1000000
+    return np_data
